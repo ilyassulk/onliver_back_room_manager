2e0798802540f3d1a14694a3a15ceb7f308e48af
ilyassulk <20044002ilya@gmail.com>
2025-04-07 16:17:59 +0300

    add room create, delete, get

---
+++ room_manager/src/main/java/ru/onliver/room_manager/controller/RoomController.java
@@ -0,0 +1,11 @@
+package ru.onliver.room_manager.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/")
+public class RoomController {
+
+}
+
