0a794203ed6c4b36e0a0afbfd3b29e4898e0b5d4
ilyassulk <20044002ilya@gmail.com>
2025-04-07 16:53:36 +0300

    add test token controller

---
+++ room_manager/src/main/java/ru/onliver/room_manager/controller/TokenController.java
@@ -0,0 +1,41 @@
+package ru.onliver.room_manager.controller;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.livekit.server.AccessToken;
+import io.livekit.server.RoomJoin;
+import io.livekit.server.RoomName;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+    @Value("${livekit.apiKey}")
+    private String apiKey;
+
+    @Value("${livekit.secret}")
+    private String secretKey;
+
+    @GetMapping
+    public ResponseEntity<?> generateToken(@RequestParam String roomName, @RequestParam String participantName) { 
+        if (roomName == null || participantName == null) {
+            return ResponseEntity.badRequest().body(Map.of("errorMessage", "roomName and participantName are required"));
+        }
+    
+        AccessToken token = new AccessToken(apiKey, secretKey); 
+        token.setName(participantName); 
+        token.setIdentity(participantName);
+        token.addGrants(new RoomJoin(true), new RoomName(roomName)); 
+    
+        return ResponseEntity.ok(Map.of("token", token.toJwt()));
+    }
+}
