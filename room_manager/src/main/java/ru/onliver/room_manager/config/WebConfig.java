583959eb8c8ddee61245ded62c7c0a87f061f912
ilyassulk <20044002ilya@gmail.com>
2025-04-08 08:56:18 +0300

    del cors

--- room_manager/src/main/java/ru/onliver/room_manager/config/WebConfig.java
+++ room_manager/src/main/java/ru/onliver/room_manager/config/WebConfig.java
@@ -7,13 +7,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost", "http://onliver.ru", "https://onliver.ru", "https://onliver.ru:8080")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
+
 } 
